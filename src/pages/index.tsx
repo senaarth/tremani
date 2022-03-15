09bff2c00da73b1576e260fae61f48b2d0de2454
Arthur Sena <senaarth@gmail.com>
2022-03-14 21:27:48 -0300

    initial

---
+++ src/pages/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+/**
+ * Home Page
+ * @return {JSX.Element}
+ */
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Head>
+        <title>Home | Vila Tremani</title>
+      </Head>
+      <h1>Vila Tremani</h1>
+    </div>
+  );
+}
---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html, body, #__next {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
