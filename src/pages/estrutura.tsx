d7a4722c7f083122894e0464e9db3b84b8510878
Arthur Sena <senaarth@gmail.com>
2022-03-27 15:35:27 -0300

    creating page estrutura

---
+++ src/pages/estrutura.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Head from "next/head";
+
+import { Agende } from "../components/Agende";
+
+import { PageContainer } from "../styles/global";
+import { ListContainer, ListItem } from "../styles/Estrutura";
+
+/**
+ * Estrutura Page
+ * @return {JSX.Element}
+ */
+
+export default function Estrutura(): JSX.Element {
+  const listItems = [
+    {
+      img: "ico-estacionamento.png",
+      label: "AMPLO ESTACIONAMENTO",
+    },
+    {
+      img: "ico-saladeproducao.png",
+      label: "SALA DE PRODUÇÃO",
+    },
+    {
+      img: "ico-arcondicionado.png",
+      label: "AR CONDICIONADO",
+    },
+    {
+      img: "ico-acessibilidade.png",
+      label: "ACESSIBILIDADE EM TODAS AS ÁREAS",
+    },
+    {
+      img: "ico-vestiariostaff.png",
+      label: "VESTIÁRIO PARA STAFF",
+    },
+    {
+      img: "ico-gerador.png",
+      label: "GERADOR",
+    },
+    {
+      img: "ico-salavip.png",
+      label: "Sala vip/camarim",
+    },
+    {
+      img: "ico-cozinha.png",
+      label: "COZINHA EQUIPADA",
+    },
+    {
+      img: "ico-agua.png",
+      label: "ÁGUA DE RE-USO PARA REGA DOS JARDINS E LIMPEZA",
+    },
+    {
+      img: "ico-pet.png",
+      label: "PET FRIENDLY",
+    },
+    {
+      img: "ico-chapelaria.png",
+      label: "chapelaria",
+    },
+  ];
+
+  return (
+    <PageContainer>
+      <Head>
+        <title>Estrutura | Vila Tremani</title>
+      </Head>
+      <img src="/images/img05.png" alt="" className="banner" />
+      <h1>Estrutura</h1>
+      <p style={{ marginBottom: "1.5rem" }}>
+        Construído no alto de um vale, em um terreno de mais de 10 mil metros
+        quadrados, o Vila Tremani é um espaço de eventos que foi cuidadosamente
+        projetado para oferecer uma experiência única e exclusiva. Conta com
+        vários ambientes integrados oferecendo grande flexibilidade na hora de
+        planejar seu evento.
+      </p>
+      <p style={{ marginBottom: "2.5rem" }}>
+        • Amplo Foyer
+        <br />
+        • Salão Principal com pé direito de 6 metros
+        <br />
+        • Varanda com 12 metros de vão livre integrada ao gramado
+        <br />• Ampla área externa
+      </p>
+      <ListContainer>
+        {listItems?.map((item) => {
+          return (
+            <ListItem>
+              <img src={`/images/${item.img}`} alt={item.label} />
+              <p>{item.label}</p>
+            </ListItem>
+          );
+        })}
+      </ListContainer>
+      <Agende />
+    </PageContainer>
+  );
+}
---
+++ src/styles/Estrutura.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+  width: 90%;
+  max-width: 60rem;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+
+  margin-bottom: 8.5rem;
+
+  @media (min-width: 1067px) {
+    justify-content: flex-start;
+  }
+`;
+
+export const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 1.5rem;
+
+  p {
+    color: black;
+    font-size: 1rem;
+    line-height: 1.25rem;
+    margin: 0;
+    text-align: left;
+    margin-left: 1.25rem;
+    width: 10rem;
+    text-transform: uppercase;
+  }
+
+  img {
+    width: 3.25rem;
+    height: 3.25rem;
+  }
+
+  @media (max-width: 630px) {
+    width: 100%;
+  }
+
+  @media (min-width: 1067px) {
+    width: 30%;
+    justify-content: flex-start;
+  }
+`;
