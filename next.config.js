6c3b6c0f10a88929afa6f4693f3a2e5005bf3f9d
senaarth <senaarth@gmail.com>
2023-02-19 21:05:13 -0300

    hsts support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*{/}?",
+        headers: [
+          { key: "Content-Security-Policy", value: "script-src 'self'" },
+          { key: "X-Frame-Options", value: "SAMEORIGIN" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "same-origin" },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+          {
+            key: "access-control-allow-origin",
+            value: "https://tremani.vercel.app/",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(), microphone=(), camera=()",
+          },
+        ],
+      },
+    ];
+  },
 };
